c88c36812b2bcc8aa0adf3f376d568c6cca0e0b2
DevPolar003 <developolar003@gmail.com>
2024-01-15 16:48:17 -0300

    Manha em hallStatt

---
+++ Script.js
@@ -0,0 +1,5 @@
+  window.alert("so cold")
+  window.confirm("Want to continue??")
+  window.confirm("You're sure you want to continue???")
+  window.confirm("You can come in but you have been warned.😳")
+  
